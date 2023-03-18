8a147c060c73f4d7e4dc578fcdf2d0817871ab2b
Bohdan Boboshko <bogdanbobosko0@gmail.com>
2023-03-18 18:37:32 +0200

    Slider JS

--- projectsToTrain/Model.js
+++ projectsToTrain/Model.js
@@ -1,34 +1,77 @@
 //    file:///C:/Users/Acer/Desktop/PROJECTS_PHP_JS/JS_train/projectsToTrain/View.html
 
-class ModalWindow
+class Slider
 {
-     buttonOpen = document.getElementById('btn-open');
-     showWindow = document.getElementById('wrapper-modal');
+    btnPrev = document.getElementById('btn-prev');
+     btnNext = document.getElementById('btn-next');
+     slides = document.querySelectorAll('.slide');
+     dots = document.querySelectorAll('.dot');
+     index = 0;
 
-     btnClose = document.getElementById('btn-close');
+    // remove class active from all elements (slide) and add class active to current element
+     activeSlide = (number) => {
+    for (let i = 0; i < this.slides.length; i++){
+        this.slides[i].classList.remove('active');
+    }
+    this.slides[number].classList.add('active');
+}
 
-     overlay = document.getElementById('overlay');
+     activeDot = (number) => {
+    for (let i = 0; i < this.dots.length; i++){
+        this.dots[i].classList.remove('active');
+    }
+    this.dots[number].classList.add('active');
+}
 
-     openModalWindow = () => {
-         this.buttonOpen.addEventListener('click', () =>{
-             this.showWindow?.classList.add('active');
-         });
-     }
+     prepareCurrentSlide = (index) =>{
+    this.activeSlide(index);
+    this.activeDot(index);
+}
 
-    closeModalWindow = () => {
-        this.btnClose.addEventListener('click', () =>{
-            this.showWindow?.classList.remove('active');
-        });
-
-        this.overlay.addEventListener('click', () =>{
-            this.showWindow?.classList.remove('active');
-        });
+ nextSlide = () => {
+    if(this.index === this.slides.length -1){
+        this.index = 0;
+        this.prepareCurrentSlide(this.index);
+    }else{
+        this.index++;
+        this.prepareCurrentSlide(this.index);
     }
 }
 
-const modalWindow = new ModalWindow();
-modalWindow.openModalWindow();
-modalWindow.closeModalWindow();
+ prevSlide = () => {
+    if(this.index === 0){
+        this.index = this.slides.length -1;
+        this.prepareCurrentSlide(this.index);
+    }else{
+        this.index--;
+        this.prepareCurrentSlide(this.index);
+    }
+}
+
+// switches pictures by clicking on a point
+dotSlider = () => {
+    this.dots.forEach((item, indexDot) => {
+        item.addEventListener('click', () =>{
+            this.index = indexDot;
+            this.prepareCurrentSlide(this.index);
+        })
+    })
+}
+
+    nextBtn = () => {
+        this.btnNext.addEventListener('click', this.nextSlide);
+    }
+
+    prevBtn = () => {
+        this.btnPrev.addEventListener('click', this.prevSlide);
+    }
+}
+
+const slider = new Slider();
+slider.nextBtn();
+slider.prevBtn();
+slider.dotSlider();
+
 
 
 
