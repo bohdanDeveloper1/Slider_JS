8edf7630408ea9a118663bedca1db43ee68adbc5
Bohdan Boboshko <bogdanbobosko0@gmail.com>
2023-03-18 11:14:23 +0200

    Modal window JS

--- projectsToTrain/Model.js
+++ projectsToTrain/Model.js
@@ -1,45 +1,34 @@
-// завдання з TABS
-//  file:///C:/Users/Acer/Desktop/JS_train/projectsToTrain/View.html
-class Tabs
+//    file:///C:/Users/Acer/Desktop/PROJECTS_PHP_JS/JS_train/projectsToTrain/View.html
+
+class ModalWindow
 {
-    // tabs - вузел елементів
-    tabs = document.getElementById('tabs');
-    content = document.querySelectorAll('.content');
+     buttonOpen = document.getElementById('btn-open');
+     showWindow = document.getElementById('wrapper-modal');
 
-    // видаляю клас active в кожного елемента, приписую active поточному елементу
-    changeClass = (currentTab) =>{
-    let elements= this.tabs.children;
-    for (let i =0; i < elements.length; i++){
-        elements[i].classList.remove('active');
-    }
-    currentTab.className = 'tab-btn active';
-}
+     btnClose = document.getElementById('btn-close');
 
-    changeText = () =>{
-        this.tabs.addEventListener('click', (e)=>{
-            // отримаємо атрибут елементу  = e.target.dataset.назва_атрибуту;
-            // e.target - це ссилка на поточний елемент (на який я клікнув)
-            const currentTab = e.target.dataset.btn;
-            console.log(currentTab);
-            this.changeClass(e.target);
-            for(let i = 0; i < this.content.length; i++){
-                this.content[i].classList.remove('active');
-                if (this.content[i].dataset.content === currentTab){
-                    this.content[i].classList.add('active');
-                }
-            }
+     overlay = document.getElementById('overlay');
+
+     openModalWindow = () => {
+         this.buttonOpen.addEventListener('click', () =>{
+             this.showWindow?.classList.add('active');
+         });
+     }
+
+    closeModalWindow = () => {
+        this.btnClose.addEventListener('click', () =>{
+            this.showWindow?.classList.remove('active');
+        });
+
+        this.overlay.addEventListener('click', () =>{
+            this.showWindow?.classList.remove('active');
         });
     }
 }
 
-const tabs = new Tabs();
-tabs.changeText();
-
-
-
-
-
-
+const modalWindow = new ModalWindow();
+modalWindow.openModalWindow();
+modalWindow.closeModalWindow();
 
 
 
